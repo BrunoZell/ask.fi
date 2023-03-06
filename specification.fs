1794287e9b3511761371982049ead463495264b2
Bruno Zell <contact@brunozell.com>
2023-03-06 05:23:52 +0100

    add user perspective

--- specification.fs
+++ specification.fs
@@ -1,6 +1,5 @@
 module AskFi
 
-type DagAddress = DagAddress of byte list
 type User = DagAddress
 
 type Commitment = {
@@ -22,9 +21,27 @@ type Proposal = {
     Alternative: ChangeOfPlan
 }
 
+// In reference to a perspective, given a situation, evaluates to either a match or not
 type ObservationPredicate = struct end
 type InvalidationCondition = struct end
 
+// The declaration of valuing certain happenings
 type Ask =
     | Attraction of ObservationPredicate * InvalidationCondition
     | Aversion of ObservationPredicate * InvalidationCondition
+
+// Groups all users asks into a set that is atomically updated
+type ValueSet = ValueSet of Ask list // v> All asks must come from the same user to form a value set
+
+type DagAddress = DagAddress of byte list
+type User = {
+    Address: DagAddress
+} with
+    /// User proposes an alternative plan for another user
+    member Propose(p: Proposal)
+
+    /// User commits to an updated plan
+    member Commit(latest: ChangeOfPlan)
+    
+    /// User declares a new value set
+    member Declare(value: ValueSet)
