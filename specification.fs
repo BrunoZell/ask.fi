ab6b60979bd8f417d77d3c19a9f208b8661683bf
Bruno Zell <contact@brunozell.com>
2023-03-10 21:07:17 +0100

    refactor F# spac to align better with existing trading system

--- specification.fs
+++ specification.fs
@@ -1,61 +1,65 @@
 module AskFi
 
-type Decision = Action | Inaction
+module Messages =
+    /// Translates into IEML phrase "$user do $action when $trigger matches $user.perspective"
+    type Commitment = {
+        /// Arbitrary IEML verb-phrase, with related user as subject.
+        Action: unit
 
-/// Translates into IEML phrase "$user do $action when $trigger matches $user.perspective"
-type Commitment = {
-    /// Arbitrary IEML verb-phrase, with related user as subject.
-    Action: unit
+        // Predicate on users perspective
+        Trigger: (UserReflection, WorldState) -> Decision
+    }
+    and Decision = Initiate | Inaction
 
-    // Predicate on users perspective
-    Trigger: (UserReflection, WorldState) -> Decision
-}
+    type ChangeOfPlan = {
+        /// Users previous 'Change Of Plan', forming a linked list
+        Previous: ChangeOfPlan
+        Added: Commitment list
+        Removed: Commitment list // v> Must be included in the previous state snapshot
+    }
 
-type ChangeOfPlan = {
-    /// Users previous CoP, forming a linked list
-    Previous: ChangeOfPlan
-    Added: Commitment list
-    Removed: Commitment list // v> Must be included in the previous state snapshot
-}
-
-/// Send & signed by 'Proposer', proposing an alternative coordination plan
-type Proposal = {
-    Proposer: User
-    Recipient: User
-    Alternative: ChangeOfPlan
-}
-
-// In reference to a perspective, given a situation, evaluates to either a match or not
-type ObservationPredicate = struct end
-type InvalidationCondition = struct end
-
-// The declaration of valuing certain happenings
-type Ask =
-    | Attraction of ObservationPredicate * InvalidationCondition
-    | Aversion of ObservationPredicate * InvalidationCondition
-
-// Groups all users asks into a set that is atomically updated
-type ValueSet = ValueSet of Ask list // v> All asks must come from the same user to form a value set
-
-type DagAddress = DagAddress of byte list
-type User = {
-    Address: DagAddress
-} with
-    /// User proposes an alternative plan for another user
-    member Propose(p: Proposal)
-
-    /// User commits to an updated plan
-    member Commit(latest: ChangeOfPlan)
+    /// Send & signed by 'Proposer', proposing an alternative coordination plan
+    type Proposal = {
+        Proposer: User
+        Recipient: User
+        Alternative: ChangeOfPlan
+    }
     
-    /// User declares a new value set
-    member Declare(value: ValueSet)
+    // In reference to a perspective, given a situation, evaluates to either a match or not
+    type ObservationPredicate = UserReflection -> WorldState -> bool
+    type InvalidationCondition = UserReflection -> WorldState -> bool
 
-type UserStateSnapshot = {
-    Values: ValueSet
-    Commitments: Commitment list
-    Situation: Situation
-}
+    // The declaration of valuing certain happenings
+    type Ask =
+        | Attraction of ObservationPredicate * InvalidationCondition
+        | Aversion of ObservationPredicate * InvalidationCondition
 
-type NetworkStateSnapshot = {
-    Users: Map<DagAddress, UserStateSnapshot>
-}
+    // Groups all users asks into a set that is atomically updated
+    type ValueSet = ValueSet of Ask list // v> All asks must come from the same user to form a value set
+
+module UserPerspective =
+    open AskFi.Messages
+
+    /// Represents a unique user in the network and the protocol messages available to send
+    type User = {
+        DagAddress: byte list
+    } with
+        /// User commits to an updated plan
+        abstract member Commit : ChangeOfPlan -> unit
+
+        /// User proposes an alternative plan for another user
+        abstract member Propose : Proposal -> unit
+
+        /// User declares a new value set
+        abstract member Declare : ValueSet -> unit
+
+    type UserStateSnapshot = {
+        Values: ValueSet
+        Commitments: Commitment list
+        Situation: Situation
+    }
+
+    /// Groups state snapshots of multiple users
+    type NetworkStateSnapshot = {
+        Users: Map<DagAddress, UserStateSnapshot>
+    }
